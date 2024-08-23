6d99917da5e04770329d1d1f594e88ab6cba70f0
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-08-23 21:10:32 +0300

    Header component added

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as Logo } from "./logo.svg";
+
+// Icons
+export { default as IconMenu } from "./icons/menu.svg";
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,7 +1,85 @@
+// Assets
+import { IconMenu, Logo } from "@/assets/images";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header id="header" className="header-default">
+      <div className="px_15 lg-px_40">
+        <div className="row wrapper-header align-items-center">
+          <div className="col-md-4 col-3 tf-lg-hidden">
+            <a role="button">
+              <img src={IconMenu} alt="menu" className="icon-menu" />
+            </a>
+          </div>
+
+          <div className="col-xl-3 col-md-4 col-6">
+            <Link to="/" className="logo-header">
+              <img src={Logo} alt="logo" className="logo" />
+            </Link>
+          </div>
+
+          <div className="col-xl-6 tf-md-hidden">
+            <nav className="box-navigation text-center">
+              <ul className="box-nav-ul d-flex align-items-center justify-content-center gap-30">
+                <li className="menu-item">
+                  <Link to="/" className="item-link">
+                    Home
+                  </Link>
+                </li>
+
+                <li className="menu-item">
+                  <Link to="/shop" className="item-link">
+                    Shop
+                  </Link>
+                </li>
+
+                <li className="menu-item">
+                  <Link to="/about" className="item-link">
+                    About us
+                  </Link>
+                </li>
+
+                <li className="menu-item">
+                  <Link to="/contact" className="item-link">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="col-xl-3 col-md-4 col-3">
+            <ul className="nav-icon d-flex justify-content-end align-items-center gap-20">
+              <li className="nav-search">
+                <a role="button" className="nav-icon-item">
+                  <i className="icon icon-search"></i>
+                </a>
+              </li>
+
+              <li className="nav-account">
+                <a role="button" className="nav-icon-item">
+                  <i className="icon icon-account"></i>
+                </a>
+              </li>
+
+              <li className="nav-wishlist">
+                <a role="button" className="nav-icon-item">
+                  <i className="icon icon-heart"></i>
+                  <span className="count-box">0</span>
+                </a>
+              </li>
+
+              <li className="nav-cart">
+                <a role="button" className="nav-icon-item">
+                  <i className="icon icon-bag"></i>
+                  <span className="count-box">0</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
