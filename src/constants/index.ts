17087517a12d9e62299b807f986bd40677b1d7e8
nihad memmedzade <nihad.mmmdzad@bk.ru>
2024-08-24 17:53:53 +0400

    Social Media details added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,9 @@ export const contactDetails = {
   address: "1234 Fashion Street, Suite 567,  New York, NY 10001",
   email: "info@fashionshop.com",
   phone: "(212) 555-1234",
+  tiktok :"https://www.tiktok.com/en/",
+  instagram:"https://www.instagram.com",
+  facebook:"https://www.facebook.com",
+  x:"https://x.com/",
+  pinterest:"https://www.pinterest.com/"
 };
