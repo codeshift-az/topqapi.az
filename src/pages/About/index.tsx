81244b20d57511ed1d052eaec02bec65cc864c59
Nihad-memmedzade <nihad.mmmdzad@bk.ru>
2024-08-26 17:02:25 +0400

    About Title component added

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,31 @@
+// Components
+
+import Layout from "@/components/Layout";
+
+// Components About
+
+import AboutBanner from "./AboutBanner";
+import AboutTitle from "./AboutTitle";
+import AboutCard1 from "./AboutCard1";
+import AboutCard2 from "./AboutCard2";
+import AboutSection from "./AboutSection";
+import ReviewSlider from "./ReviewSlider";
+import ShopGramSlider from "./ShopGramSlider";
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <Layout>
+      <div id="wrapper">
+        <AboutBanner />
+        <AboutTitle />
+        <AboutCard1 />
+        <AboutCard2 />
+        <AboutSection />
+        <ReviewSlider />
+        <ShopGramSlider />
+      </div>
+    </Layout>
+  );
 };
 
 export default About;
