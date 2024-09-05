c2e6ba8613548be5c3866cf6d7d6b0824f051a29
nihad memmedzade <nihad.mmmdzad@bk.ru>
2024-09-05 21:42:55 +0400

    Assets Icons uptaded

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,8 @@
 // Icons
 export { default as IconMenu } from "./icons/menu.svg";
 export { default as IconQuote } from "./icons/quote.svg";
+export {default as CarIcon} from "./icons/car.svg"
+
 
 // Logo
 export { default as Logo } from "./icons/logo.svg";
@@ -10,6 +12,8 @@ export { default as PageTitleBackground } from "./background/page-title.png";
 
 // Products
 export { default as ProductImage1 } from "./products/product-1.jpg";
+export { default as ViewCartImg } from "./products/white-2.jpg";
+
 
 // Categories
 export { default as CategoryImage1 } from "./categories/category-1.jpg";
@@ -21,3 +25,12 @@ export { default as AboutHistory } from "./about/history.jpg";
 export { default as AboutMission1 } from "./about/mission-1.jpg";
 export { default as AboutMission2 } from "./about/mission-2.jpg";
 export { default as AboutGallery1 } from "./about/gallery-1.jpg";
+
+// Payment Icons
+export {default as VisaIcon} from "./icons/visa.svg"
+export {default as PaypalIcon} from "./icons/paypal.svg"
+export {default as MasterIcon} from "./icons/mastercard.svg"
+export {default as AmericanIcon} from "./icons/americanExpres.svg"
+export {default as AmazonIcon} from "./icons/amazon.svg"
+
+
